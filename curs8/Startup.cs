413de465334df07fcef31b076aa3be8d47803647
unknown <ana.opra@XDR117.xoomworks.local>
2017-01-27 19:40:07 +0200

    Curs 8 Ex1

---
+++ curs8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(curs8.Startup))]
+namespace curs8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
